d31f1de459cc694ab62f4af1191643bb45b34d27
Michelle Ruiter <mruiter4@gmail.com>
2020-05-04 16:53:17 -0700

    Completed word_count function

--- dicts-word-count/wordcount.py
+++ dicts-word-count/wordcount.py
@@ -1,1 +1,21 @@
-# put your code here.
+def count_words(filename):
+    """count the # of times each word appears in the file"""
+    word_count = {}
+
+    file = open(filename)
+    
+    for line in file:
+        line = line.rstrip()
+
+        word_list = line.split(" ")
+
+        for word in word_list:
+            word_count[word] = word_count.get(word, 0) + 1
+
+    file.close()
+
+  
+    for word, count in word_count.items():
+        print(f'{word} : {count}')
+
+
