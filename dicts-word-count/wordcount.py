c866c402913fb7b17a655fcfc0984a42f80941c3
Michelle Ruiter <mruiter4@gmail.com>
2020-05-04 18:28:56 -0700

    Removed punctuation and made case insensitive

--- dicts-word-count/wordcount.py
+++ dicts-word-count/wordcount.py
@@ -1,3 +1,5 @@
+import string
+
 def count_words(filename):
     """count the # of times each word appears in the file"""
     word_count = {}
@@ -6,16 +8,16 @@ def count_words(filename):
     
     for line in file:
         line = line.rstrip()
-
-        word_list = line.split(" ")
+        word_list = line.split()
 
         for word in word_list:
+            word = word.strip(string.punctuation)
+            word = word.lower()
             word_count[word] = word_count.get(word, 0) + 1
 
-    file.close()
-
-  
     for word, count in word_count.items():
         print(f'{word} : {count}')
 
+    file.close()
 
+    
